88041a6b1cc5f1baf0c824b0b65751e2cc3f3b51
luluw <shashinmhrzn@gmail.com>
2023-11-07 14:53:28 +0545

    Update: BackEnd Demoshowcase

--- app.py
+++ app.py
@@ -1,47 +1,70 @@
 import streamlit as st
 import pickle
+import numpy as np
 
-pipe = pickle.load(open('pipe.pkl','rb'))
-df=pickle.load(open('df.pkl','rb'))
+pipe = pickle.load(open('model/pipe_object.pkl', 'rb'))
+df = pickle.load(open('model/laptop_data.pkl', 'rb'))
 st.title('Laptop Price Prediction')
 
 # Company Brand
-company = st.selectbox('Brand',df['Company'].unique())
+company = st.selectbox('Brand', df['Company'].unique())
 
 # Laptop Type
-company = st.selectbox('Type',df['TypeName'].unique())
+type = st.selectbox('Type', df['TypeName'].unique())
 
 # RAM
-company = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64].unique())
+ram = st.selectbox('RAM(in GB)', [4, 6, 8, 12, 16, 24, 32, 64], index=2)
 
 # Weight
-weight = st.number_input()
+weight = st.number_input('Weight of the Laptop(in Kg)', min_value=1.0, max_value=2.8, value=1.8, step=0.1)
 
 # TouchScreen
-touchscreen = st.selectbox('TouchScreen',['Yes', 'No'])
+touchscreen = st.selectbox('TouchScreen', ['No', 'Yes'])
 
 # Display
-ips = st.selectbox('IPS',['Yes', 'No'])
+ips = st.selectbox('IPS', ['Yes', 'No'])
 
 # Screen Size
-screen_size = st.number_input('Screen Size')
+screen_size = st.selectbox('Screen Size (in Inch)', [13.3, 15.6, 17.3])
 
 # Resolution
-resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900',
-                                               '3840x2160','3200x1800','2880x1800',
-                                               '2560x1600','2560x1440','2304x1440'])
+resolution = st.selectbox('Screen Resolution', ['1920 x 1080', '1366 x 768', '1600 x 900',
+                                                '3840 x 2160', '3200 x 1800', '2880 x 1800',
+                                                '2560 x 1600', '2560 x 1440', '2304 x 1440'])
 
 # CPU
-cpu = st.selectbox('CPU',df['Cpu brand'].unique())
+cpu = st.selectbox('CPU', df['Cpu Brand'].unique())
 
 # HDD
-hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+hdd = st.selectbox('HDD(in GB)', [0, 256, 512, 1024, 2048])
 
 # SSD
-ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+ssd = st.selectbox('SSD(in GB)', [0, 128, 256, 512, 1024])
 
 # GPU
-gpu = st.selectbox('GPU',df['Gpu brand'].unique())
+gpu = st.selectbox('GPU', df['GpuBrand'].unique())
 
 # OS
-os = st.selectbox('OS',df['os'].unique())
+os = st.selectbox('OS', df['os'].unique())
+
+if st.button('Predict Price'):
+    # Preprocess input features
+    touchscreen = int(touchscreen == 'Yes')
+    ips = int(ips == 'Yes')
+    X_res, Y_res = map(int, resolution.split('x'))
+    ppi = ((X_res**2 + Y_res**2)**0.5) / screen_size
+
+    # Create the query array and make the prediction
+    query = np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os]).reshape(1, -1)
+    predicted_price = int(np.exp(pipe.predict(query)[0]))
+
+    # Display the result
+    st.title(f"\nPrice: {predicted_price*0.012} USD")
+
+
+# Note
+st.write("""
+<span style="color:green">Please note the dataset was collected from Amazon in 2017-18.<br>
+Furthermore, only 1301 samples were collected, so the price may be inaccurate.</span>
+""", unsafe_allow_html=True)
+
